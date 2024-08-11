95ffe7a07a61690db80a728471fad570a2f8a4c8
Jason Song <jasonsong.93@gmail.com>
2024-08-12 00:49:23 +1000

    Updating to only have dotilfes for nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,49 @@
-require("config.lazy")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
 
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+if vim.v.shell_error ~= 0 then
+vim.api.nvim_echo({
+    { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+    { out, "WarningMsg" },
+    { "\nPress any key to exit..." },
+    }, true, {})
+  vim.fn.getchar()
+os.exit(1)
+  end
+  end
+  vim.opt.rtp:prepend(lazypath)
+
+  local plugins = {
+    { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true},
+    {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+  }
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },
+})
+
+require("gruvbox").setup()
+vim.cmd.colorscheme "gruvbox"
