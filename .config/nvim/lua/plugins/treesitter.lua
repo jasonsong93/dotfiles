e9d7b642543e8d565d48891291de6fd240b1a4c3
Jason Song <jasonsong.93@gmail.com>
2024-08-14 00:41:09 +1000

    Updated conf

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,14 @@
-return 
-  {
-    "nvim-treesitter/nvim-treesitter", 
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "markdown" },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-      require("telescope").load_extension("ui-select")
-    end
-  }
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      auto_install = true,
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+    require("telescope").load_extension("ui-select")
+  end,
+}
--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -2,4 +2,8 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set relativenumber")
+vim.cmd("set numberwidth=2")
 
+
+vim.opt.swapfile = false
