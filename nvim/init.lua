3490584fa4f86125290516b3acc8790ad91fe7df
Jason Song <jasonsong.93@gmail.com>
2025-01-13 23:07:54 +1100

    Update

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,17 +5,17 @@ vim.cmd("set shiftwidth=4")
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -27,11 +27,28 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 local plugins = {
-	{ "rose-pine/neovim", as = "rose-pine" }
+	{ "rose-pine/neovim", as = "rose-pine" },
+	'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	dependencies = { 'nvim-lua/plenary.nvim' },
+	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
 }
 local opts = {}
 
 require("lazy").setup(plugins, opts)
 
+-- Telescope setup
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- Treesitter setup
+local config = require("nvim-treesitter.configs")
+config.setup({
+	ensure_installed = { "lua" },
+})
+
+-- Colour schemes
 require("rose-pine").setup()
 vim.cmd.colorscheme "rose-pine"
