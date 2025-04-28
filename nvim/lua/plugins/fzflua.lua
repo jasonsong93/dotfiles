1e22e4eb1c265b1f128702a724363dfaf77eb5c2
Jason <jasonsong.93@gmail.com>
2025-04-29 02:34:10 +1000

    Updated

---
+++ nvim/lua/plugins/fzflua.lua
@@ -0,0 +1,36 @@
+-- use :lua require('fzf-lua').builtin() to show all the different fzf
+-- for example there's a git one
+return {
+  "ibhagwan/fzf-lua",
+  -- optional for icon support
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  -- or if using mini.icons/mini.nvim
+  -- dependencies = { "echasnovski/mini.icons" },
+  opts = {},
+  keys = {
+    { 
+      "<leader>ff", function() require("fzf-lua").files() end,
+      desc = "Find files in current working directory"
+    },
+    { 
+      "<leader>fg", function() require("fzf-lua").live_grep() end,
+      desc = "Find by grepping in project directory"
+    },
+    { 
+      "<leader>fc", function() require("fzf-lua").files({cwd=vim.fn.stdpath("config")}) end,
+      desc = "Find in neovim configuration" 
+    },
+    { 
+      "<leader>fh", function() require("fzf-lua").helptags() end,
+      desc = "[F]ind [H]elp"
+    },
+    { 
+      "<leader>fk", function() require("fzf-lua").keymaps() end,
+      desc = "[F]ind [K]eymaps"
+    },
+    { 
+      "<leader>fo", function() require("fzf-lua").oldfiles() end,
+      desc = "[F]ind [O]ld files"
+    },
+  }
+}
--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,25 +1,33 @@
-return {
-	{
-		'stevearc/oil.nvim',
-		dependencies = { { "echasnovski/mini.icons", opts = {} } },
-		config = function()
-			local oil = require 'oil'
-			oil.setup {
-				columns = { 'icon' },
-				view_options = {
-					show_hidden = true,
-				},
-				delete_to_trash = true, -- Deletes to trash
-				skip_confirm_for_simple_edits = true,
-				use_default_keymaps = false,
-				keymaps = {
-					['<CR>'] = 'actions.select',
-					['-'] = 'actions.parent',
-					["`"] = { "actions.cd", mode = "n" },
-					["<C-l>"] = "actions.refresh",
-				},
-			}
-			vim.keymap.set('n', '_', require('oil').toggle_float)
-		end,
-	},
+return { 
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    view_options = {
+      show_hidden = true
+    }
+  },
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {
+    keymaps = {
+      ["g?"] = { "actions.show_help", mode = "n" },
+      ["<CR>"] = "actions.select",
+      ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+      ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+      ["<C-t>"] = { "actions.select", opts = { tab = true } },
+      ["<C-p>"] = "actions.preview",
+      ["<C-c>"] = { "actions.close", mode = "n" },
+      ["<C-l>"] = "actions.refresh",
+      ["-"] = { "actions.parent", mode = "n" },
+      ["_"] = { "actions.open_cwd", mode = "n" },
+      ["`"] = { "actions.cd", mode = "n" },
+      ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+      ["gs"] = { "actions.change_sort", mode = "n" },
+      ["gx"] = "actions.open_external",
+      ["g."] = { "actions.toggle_hidden", mode = "n" },
+      ["g\\"] = { "actions.toggle_trash", mode = "n" },
+    },} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
 }
