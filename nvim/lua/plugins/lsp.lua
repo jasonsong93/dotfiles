c59096c4fde90558e9fb8e7fd35aaa7b36675116
Jason <jasonsong.93@gmail.com>
2025-04-29 02:43:32 +1000

    Update nvim config: 2025-04-29 02:43:32

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -162,7 +162,6 @@ return {
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       bashls = {},
-      marksman = {},
       -- clangd = {},
       -- gopls = {},
       -- pyright = {},
