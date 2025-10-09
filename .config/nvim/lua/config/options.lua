82a12882ab5734866be63c835f9a9784480fd22f
Jason S <12515705+jasonsong93@users.noreply.github.com>
2025-10-09 01:24:34 +0100

    removed accidental symlinks and updated lazy and opts. Use Lex! nvim command for navbar

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,12 +3,35 @@ vim.g.maplocalleader = " "
 
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.cursorline = true
+
 vim.opt.mouse = "a"
+
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.hlsearch = false
+
 vim.opt.wrap = false
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+vim.opt.undofile = true
+
 vim.opt.termguicolors = true
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+vim.opt.scrolloff = 5
+
+vim.g.netrw_winsize = 25
+vim.g.netrw_liststyle = 3
+vim.g.netrw_altv = 1
