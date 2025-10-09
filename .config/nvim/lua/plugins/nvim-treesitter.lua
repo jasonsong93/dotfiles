ae078fe94792c0f58cc462c588edbe0ee5d65a4d
Jason S <12515705+jasonsong93@users.noreply.github.com>
2025-10-09 02:26:27 +0100

    Added which-key to assists for learning and fzf-lua for grep and fzf

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -27,6 +27,16 @@ return {
     indent = {
       enable = true,
     },
+
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",   },
+    },
+
   },
   config = function(_, opts)
     require("nvim-treesitter.configs").setup(opts)
